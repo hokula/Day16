1f4009f0baf7eeb1ce435dbc630c8e51988a75f8
hokula <adelhokula18@gmail.com>
2021-03-23 18:55:33 +0100

    Getters and Setters - Controlling the Access to an Object

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,15 +1,14 @@
 class Book {
-    constructor(title) {
-        this._title = title;
-    }
-    get title() {
-        return this._title;
-    }
-    set title(newTitle) {
-        this._title = newTitle;
-    }
+  constructor(title) {
+    this._title = title;
+  }
+  get title() {
+    return this._title;
+  }
+  set title(newTitle) {
+    this._title = newTitle;
+  }
 }
-
 
 const littlePrince = new Book("The Little Prince");
 console.log(littlePrince.title);
